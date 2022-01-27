ae75bb76bea703c08276ccd185541e76315d4812
Eduardo Pi;a <eduardo_pgdc@sssirsa.com>
2022-01-27 11:25:14 -0600

    func create azure funct profile

---
+++ profile/index.js
@@ -0,0 +1,30 @@
+const mongodb = require("mongodb");
+let mongo_client = null;
+
+const connection_mongoDB =
+  "mongodb+srv://devops:59LzYD00s3q9JK2s@cluster0-qrhyj.mongodb.net?retryWrites=true&w=majority";
+const MONGO_DB_NAME = "management";
+
+const validator = require("validator");
+
+// constants environment
+const AZURE_STORAGE_CONNECTION_STRING =
+  process.env["AUTH_AZURE_STORAGE_CONNECTION_STRING"];
+const STORAGE_ACCOUNT_NAME = process.env["AZURE_STORAGE_ACCOUNT_NAME"];
+
+module.exports = function (context, req) {
+  switch (req.method) {
+    case "GET":
+      GET_profile();
+      break;
+    case "POST":
+      POST_profile();
+      break;
+    case "PUT":
+      PUT_profile();
+      break;
+    default:
+      notAllowed();
+      break;
+  }
+};
