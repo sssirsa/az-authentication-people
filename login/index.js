7f576f955b5da8ba7d75f9feccc66a0765a79d0f
Eduardo Pi;a <eduardo_pgdc@sssirsa.com>
2022-01-27 14:49:55 -0600

    update login

--- login/index.js
+++ login/index.js
@@ -1,202 +1,203 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 let mongo_client = null;
 
-const connection_mongoDB = process.env["connection_mongoDB"];
-const MONGO_DB_NAME = process.env['MONGO_DB_NAME'];
+const connection_mongoDB =
+  "mongodb+srv://devops:59LzYD00s3q9JK2s@cluster0-qrhyj.mongodb.net?retryWrites=true&w=majority";
+const MONGO_DB_NAME = "management";
 
-const crypto = require('crypto');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const SECRET_JWT_SEED = "e201994dca9320fc94336603b1cfc970";
 
 module.exports = function (context, req) {
+  switch (req.method) {
+    case "POST":
+      POST_login();
+      break;
+    default:
+      notAllowed();
+      break;
+  }
 
-    switch (req.method) {
-        case "POST":
-            POST_login();
-            break;
-        default:
-            notAllowed();
-            break;
-    }
+  function notAllowed() {
+    context.res = {
+      status: 405,
+      body: "Method not allowed",
+      headers: { "Content-Type": "application/json" },
+    };
+    context.done();
+  }
 
-    function notAllowed() {
+  async function POST_login() {
+    let userName = req.body["username"];
+    let userPassword = req.body["password"];
+    try {
+      validate();
+      console.log("validate pass");
+      let user = await searchUser();
+      if (bcrypt.compareSync(userPassword, user.password)) {
+        const token = await generarJWT(user._id, userName);
+        const person = await searchPerson(user["person_id"].toString());
+        let response = {
+          access_token: token,
+          person,
+        };
+        console.log(response);
         context.res = {
-            status: 405,
-            body: "Method not allowed",
-            headers: {
-                'Content-Type': 'application/json'
-            }
+          status: 200,
+          body: response,
+          headers: {
+            "Content-Type": "application/json",
+          },
         };
         context.done();
+      } else {
+        context.res = {
+          status: 401,
+          body: "Invalid password",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        context.done();
+      }
+    } catch (error) {
+      context.res = error;
+      context.done();
     }
 
-    async function POST_login() {
-        let userName = req.body['username'];
-        let userPassword = req.body['password'];
+    //Internal functions
+    function validate() {
+      if (!userName) {
+        context.res = {
+          status: 400,
+          body: { message: "No se ha recibido el usuario" },
+          headers: { "Content-Type": "application/json" },
+        };
+        context.done();
+      }
+      if (!userPassword) {
+        context.res = {
+          status: 400,
+          body: { message: "No se ha recibido la contraseña" },
+          headers: { "Content-Type": "application/json" },
+        };
+        context.done();
+      }
+    }
 
+    async function searchUser() {
+      await createMongoClient();
+      return new Promise(function (resolve, reject) {
         try {
-            let user = await searchUser();
-            if (validatePasswordHash(user.password.passwordData, userPassword)) {
-                let fakeResponse = {
-                    access_token: user['person_id'].toString(),
-                    expires_in: 86400,
-                    token_type: "Bearer",
-                    refresh_token: user['_id'].toString(),
-                    person: await searchPerson(user['person_id'].toString())
-                };
-                context.res = {
-                    status: 200,
-                    body: fakeResponse,
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-                context.done();
-            }
-            else {
-                context.res = {
-                    status: 401,
-                    body: 'Invalid password',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-                context.done();
-            }
-        }
-        catch (error) {
-            context.res = error;
-            context.done();
-        }
-
-        //Internal functions
-        async function searchUser() {
-            await createMongoClient();
-            return new Promise(function (resolve, reject) {
-                try {
-                    mongo_client
-                        .db(MONGO_DB_NAME)
-                        .collection('users')
-                        .findOne({ username: userName },
-                            function (error, docs) {
-                                if (error) {
-                                    reject({
-                                        status: 500,
-                                        body: error.toString(),
-                                        headers: {
-                                            'Content-Type': 'application/json'
-                                        }
-                                    });
-                                    return;
-                                }
-                                if (!docs) {
-                                    reject({
-                                        status: 401,
-                                        body: 'Invalid username',
-                                        headers: {
-                                            'Content-Type': 'application/json'
-                                        }
-                                    });
-                                }
-                                resolve(docs);
-                            }
-                        );
-                }
-                catch (error) {
-                    reject({
-                        status: 500,
-                        body: error.toString(),
-                        headers: {
-                            "Content-Type": "application/json"
-                        }
-                    })
-                }
-            });
-        }
-        async function searchPerson(personId) {
-            await createMongoClient();
-            return new Promise(function (resolve, reject) {
-                try {
-                    mongo_client
-                        .db(MONGO_DB_NAME)
-                        .collection('profiles')
-                        .findOne({ _id: mongodb.ObjectID(personId) },
-                            function (error, docs) {
-                                if (error) {
-                                    reject({
-                                        status: 500,
-                                        body: error.toString(),
-                                        headers: {
-                                            'Content-Type': 'application/json'
-                                        }
-                                    });
-                                    return;
-                                }
-                                if (!docs) {
-                                    reject({
-                                        status: 401,
-                                        body: 'Not found person',
-                                        headers: {
-                                            'Content-Type': 'application/json'
-                                        }
-                                    });
-                                }
-                                resolve(docs);
-                            }
-                        );
-                }
-                catch (error) {
-                    reject({
-                        status: 500,
-                        body: error.toString(),
-                        headers: {
-                            "Content-Type": "application/json"
-                        }
-                    })
-                }
-            });
-        }
-
-        function validatePasswordHash(passwordObject, passwordString) {
-            let salt = passwordObject['salt'];
-            let calculatedPassword = sha512(passwordString, salt);
-
-            if (passwordObject.passwordHash === calculatedPassword.passwordHash) {
-                return true;
-            }
-            else {
-                return false;
-            }
-
-            //Internal functions
-
-            function sha512(password, salt) {
-                var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
-                hash.update(password);
-                var value = hash.digest('hex');
-                return {
-                    salt: salt,
-                    passwordHash: value
-                };
-            };
-
-        }
-    }
-
-    //Internal globals
-    function createMongoClient() {
-        return new Promise(function (resolve, reject) {
-            if (!mongo_client) {
-                mongodb.MongoClient.connect(connection_mongoDB, function (error, _mongo_client) {
-                    if (error) {
-                        reject(error);
-                    }
-                    mongo_client = _mongo_client;
-                    resolve();
+          mongo_client
+            .db(MONGO_DB_NAME)
+            .collection("usuarios")
+            .findOne({ username: userName }, function (error, docs) {
+              if (error) {
+                reject({
+                  status: 500,
+                  body: error.toString(),
+                  headers: { "Content-Type": "application/json" },
                 });
-            }
-            else {
-                resolve();
-            }
-        });
+                return;
+              }
+              if (!docs) {
+                reject({
+                  status: 401,
+                  body: "Invalid username",
+                  headers: { "Content-Type": "application/json" },
+                });
+              }
+              resolve(docs);
+            });
+        } catch (error) {
+          reject({
+            status: 500,
+            body: error.toString(),
+            headers: { "Content-Type": "application/json" },
+          });
+        }
+      });
     }
 
+    async function searchPerson(personId) {
+      await createMongoClient();
+      return new Promise(function (resolve, reject) {
+        try {
+          mongo_client
+            .db(MONGO_DB_NAME)
+            .collection("profiles_test")
+            .findOne(
+              { _id: mongodb.ObjectID(personId) },
+              function (error, docs) {
+                if (error) {
+                  reject({
+                    status: 500,
+                    body: error.toString(),
+                    headers: { "Content-Type": "application/json" },
+                  });
+                  return;
+                }
+                if (!docs) {
+                  reject({
+                    status: 401,
+                    body: "Not found person",
+                    headers: { "Content-Type": "application/json" },
+                  });
+                }
+                resolve(docs);
+              }
+            );
+        } catch (error) {
+          reject({
+            status: 500,
+            body: error.toString(),
+            headers: { "Content-Type": "application/json" },
+          });
+        }
+      });
+    }
+
+    async function generarJWT(uid, name) {
+      const payload = { uid, name };
+      return new Promise((resolve, reject) => {
+        jwt.sign(
+          payload,
+          SECRET_JWT_SEED,
+          {
+            expiresIn: "24h",
+          },
+          (error, token) => {
+            if (error) {
+              // todo mal
+              console.log(error);
+              reject(error);
+            } else {
+              resolve(token);
+            }
+          }
+        );
+      });
+    }
+  }
+
+  //Internal globals
+  function createMongoClient() {
+    return new Promise(function (resolve, reject) {
+      if (!mongo_client) {
+        mongodb.MongoClient.connect(
+          connection_mongoDB,
+          function (error, _mongo_client) {
+            if (error) reject(error);
+            mongo_client = _mongo_client;
+            resolve();
+          }
+        );
+      } else {
+        resolve();
+      }
+    });
+  }
 };
